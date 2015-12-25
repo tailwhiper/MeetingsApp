3e7d05ec6f0f57cdace1d2884e2d5ba33ca27aea
tailwhiper <tailwhiper@gmail.com>
2015-12-25 11:45:14 +0400

    download Short info on SwipeRefresh

--- app/src/main/java/tavi/tiki/niki/meetingsapplication/Meetings.java
+++ app/src/main/java/tavi/tiki/niki/meetingsapplication/Meetings.java
@@ -1,7 +1,6 @@
 package tavi.tiki.niki.meetingsapplication;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
--- app/src/main/java/tavi/tiki/niki/meetingsapplication/Restapi.java
+++ app/src/main/java/tavi/tiki/niki/meetingsapplication/Restapi.java
@@ -1,1 +1,19 @@
+package tavi.tiki.niki.meetingsapplication;
 
+import java.util.List;
+
+import model.Meeting;
+import model.MeetingShortInfo;
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+
+public interface Restapi {
+
+    @GET("/get/all/short")
+    public void getAllShort(Callback<List<MeetingShortInfo>> response);
+    @GET("/get/{id}")
+    public void getMeeting(@Path ("id") String id ,Callback<List<Meeting>> response);
+
+}
